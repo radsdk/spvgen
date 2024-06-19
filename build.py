55cdf3fad2ad2b06f80726a5063a50fa0d2145d3
Jerry Ma <majunlichn@outlook.com>
2024-06-19 16:44:05 +0800

    improve build

---
+++ build.py
@@ -0,0 +1,40 @@
+import os
+import sys
+import subprocess
+
+scriptRoot = os.path.dirname(os.path.realpath(__file__))
+workingDirStack = list()
+
+def pushd(dir):
+    global workingDirStack
+    workingDirStack.append(os.getcwd())
+    os.chdir(dir)
+    print("Working dir: ", os.getcwd())
+
+def popd():
+    global workingDirStack
+    os.chdir(workingDirStack.pop())
+
+def shell(command : str):
+    print("Execute: ", command)
+    subprocess.run(command, shell=True)
+
+def build_spvgen():
+    global scriptRoot
+    pushd(scriptRoot)
+    pushd("external/")
+    shell("python fetch_external_sources.py")
+    popd()
+    shell("cmake --build build/ --target spvgen --config Release")
+    popd()
+
+def main() -> int:
+    try:
+        build_spvgen()
+        return 0
+    except Exception as e:
+        print(e)
+        return -1
+
+if __name__ == '__main__':
+    sys.exit(main())
